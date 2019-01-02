573bd9a9b888a85af00f0dab571c54f6cb809d93
adieunix <adie.rock@yahoo.com>
2019-01-02 15:53:14 +0700

    updated

--- helpers/Constants.js
+++ helpers/Constants.js
@@ -1,6 +1,10 @@
+const user = encodeURIComponent('oomphuser');
+const password = encodeURIComponent('oomph123');
+const authMechanism = 'DEFAULT';
+
 const Constant = {    
     
-    DB_SERVER: 'mongodb://localhost:27017/oomph', // DB name: oomph
+    DB_SERVER: 'mongodb://'+user+':'+password+'@localhost:27017/oomph?authMechanism='+authMechanism, // DB name: oomph
     API_GET_ALL_USERS: '/get_all_users', // {start,limit}
     API_ADD_USER: '/add_user', // {name,email}
     API_CREATE_COLL: '/create_collection', // :name
